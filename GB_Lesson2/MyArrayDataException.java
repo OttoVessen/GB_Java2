f605c0f7b86f67de25d8c9e2b1b3f279d1c71461
Roman Ambarnikov <z-tula@bk.ru>
2019-04-05 12:22:00 +0300

    Домашнее задание ко второму занятию

---
+++ GB_Lesson2/MyArrayDataException.java
@@ -0,0 +1,10 @@
+package GB_Lesson2;
+
+public class MyArrayDataException extends Exception {
+
+    public MyArrayDataException (String ex){
+
+        System.out.println(ex);
+    }
+
+}
---
+++ GB_Lesson2/MyArraySizeException.java
@@ -0,0 +1,9 @@
+package GB_Lesson2;
+
+public class MyArraySizeException extends Exception {
+
+
+    public MyArraySizeException(String ex) {
+        System.out.println(ex+ "\n");
+    }
+}
